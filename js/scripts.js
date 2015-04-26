e730ed81f54ed0ad4c9bdd0d30e8546e8574a48b
kyle <icegoat01@gmail.com>
2015-04-26 13:23:22 -0700

    Add README

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,35 @@
 var queenAttack = function(queen, enemy) {
-    if (queen[0] === enemy[0]) {
-        return true;
-    }
-    if (queen[1] === enemy[1]) {
-        return true;
-    }
-    if ((queen[1] - enemy[1]) === (queen[0] - enemy[0])) {
-        return true;
-    }
-    else {
-        return false;
-    };
+
+  //horizontal/vertical align
+  if((queen[1] === enemy[1]) || (queen[0] === enemy[0])) {
+    return true;
+  }
+  //diagonal align
+  if((queen[0]-enemy[0]) === (queen[1]-enemy[1])) {
+    return true;
+  }
+  //fail
+  else {
+    return false;
+  }
 };
 
 $(document).ready(function() {
-    $("form#coords").submit(function(event) {
+  $("form#attack").submit(function(event) {
+    var queen_x = parseInt($("input#queen_x").val());
+    var queen_y = parseInt($("input#queen_y").val());
     var enemy_x = parseInt($("input#enemy_x").val());
     var enemy_y = parseInt($("input#enemy_y").val());
 
-    var queen_x = parseInt($("input#queen_x").val());
-    var queen_y = parseInt($("input#queen_y").val());
-
     var result = queenAttack([queen_x, queen_y], [enemy_x, enemy_y]);
-
-    if (!result) {
-        $(".not").text("not");
+    debugger;
+    if(result) {
+      $(".result").text("Attack hits");
+    } else {
+      $(".result").text("Attack misses");
     }
-
-    else {
-        $(".not").text("");
-    }
-
     $("#result").show();
     event.preventDefault();
 
-    });
+  });
 });
